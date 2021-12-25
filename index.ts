2e9e455a513da3161fec9df9c3a335ead153ae7e
Leonardo <ambrosioleonardo57@gmail.com>
2021-12-25 09:37:28 -0300

    wow

--- index.ts
+++ index.ts
@@ -1,1 +1,31 @@
-import Database from './src/Database'
+import fs from 'fs';
+
+type Key = string | number;
+
+export default class Database {
+  path: string;
+  
+  constructor(path: string) {
+    this.path = path;
+  }
+  
+  get json(): object {
+    return fs.existsSync(this.path) ? JSON.parse(fs.readFileSync(this.path, 'utf8')) : {};
+  }
+  
+  set json(obj: object) {
+    fs.writeFileSync(this.path, JSON.stringify(obj, undefined, 2));
+  }
+  
+  get version() {
+    return require("./package.json").version;
+  }
+
+  get name() {
+    return require("./package.json").name;
+  }
+
+  get all() {
+    return this.json;
+  }
+}
